63621a1beed4a4153ac7fecb27e1d4f6bce1856b
Daniel <135396932+d-chien@users.noreply.github.com>
2024-11-29 17:54:20 +0800

    Update LDA.py

--- LDA.py
+++ LDA.py
@@ -1,1 +1,25 @@
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+np.set_printoptions(precision = 2)
+
+# setup vectorizer and doc
+count = CountVectorizer()
+docs = np.array([
+    'The sun is shining',
+    'The weather is sweet',
+    'the sun is shining, the weather is sweet',
+    'and one and one is two',
+    'why we need to work for one third of our life'
+])
+bag = count.fit_transform(docs)
+
+# count voc and no.
+print(count.vocabulary_)
+# print no and mapping freq
+print(bag.toarray())
+
+tfidf = TfidfTransformer(use_idf = True, norm = 'l2', smooth_idf = True)
+print(tfidf.fit_transform(bag).toarray())
+
+
 
