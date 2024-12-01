f9b7ec59a785078e4d4f94baf2caad3b3a1d42fb
Daniel <daniel.famd@gmail.com>
2024-12-01 22:33:41 +0800

    finishing LDA and update README

--- LDA.py
+++ LDA.py
@@ -1,3 +1,40 @@
-import tarfile
-with tarfile.open('./ACL IMDB v1 Sentiment Analysis.tar.gz','r:gz') as tar:
-    tar.extractall()
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+from tqdm.auto import tqdm
+from time import time
+
+df = pd.read_csv('movie_data.csv', encoding = 'utf-8')
+
+# setup CountVec as bag of word, to input to LDA
+count = CountVectorizer(stop_words='english', max_df=.1,max_features = 5000)
+'''
+stop_words: built-in english stop words, so we don't have to use tf-idf for stop words
+max_df: max frequency a word appear in all docs to prevent uncatched stopwords
+max_features: limit vocabularies in a topic
+'''
+
+X = count.fit_transform(df.review.values)
+
+# setup lda
+lda = LatentDirichletAllocation(n_components=10,random_state=38, learning_method='batch')
+
+x_topics = lda.fit_transform(X)
+print(lda.components_.shape)  # (topic, vocs)
+
+n_top_words = 5
+feature_names = count.get_feature_names()  # aquire words in bag of word
+for idx, topic in enumerate(lda.components_):
+    print(f'Topic {idx}')
+    print(' '.join([feature_names[i] for i in topic.argsort()[:-n_top_words-1:-1]]))
+
+'''
+validate
+Topic 9
+horror sex gore fans scary
+'''
+
+horror = x_topics[:,9].argsort()[::-1]
+for idx, moview_idx in enumerate(horror[:3]):
+    print(f'Horror movie #{idx}')
+    print(df.review[moview_idx][:300],'...')
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 this is a practice work cited from
 [LDA](https://mycollegenotebook.medium.com/lda-latent-dirichlet-allocation-%E7%AF%84%E4%BE%8B-%E5%AF%A6%E4%BD%9C-c63081c92632)
+
+steps:
+1. do file extraction with *file_extraction.py* to get needed data from tar.gz and save as **movie_data.csv**
+2. do data cleaning with *data_cleaning.py* to drop html symbols, other puctuation marks, and extract emoji
+3. do LDA with *LDA.py* to get topics and test last topic to see if it's fine.
