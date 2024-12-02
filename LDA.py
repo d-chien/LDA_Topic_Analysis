169f0ae1c6172a3103f27635ad9d87a64c2d6791
Daniel <135396932+d-chien@users.noreply.github.com>
2024-12-02 15:00:32 +0800

    add footnotes to better understand the process

--- LDA.py
+++ LDA.py
@@ -19,14 +19,14 @@ X = count.fit_transform(df.review.values)
 # setup lda
 lda = LatentDirichletAllocation(n_components=10,random_state=38, learning_method='batch')
 
-x_topics = lda.fit_transform(X)
+x_topics = lda.fit_transform(X)  # get possibility to identify as topic n for each review (array(review_count, topics_count))
 print(lda.components_.shape)  # (topic, vocs)
 
 n_top_words = 5
 feature_names = count.get_feature_names()  # aquire words in bag of word
-for idx, topic in enumerate(lda.components_):
+for idx, topic in enumerate(lda.components_):  # lda.components_ would return weight of each word in bag for each topic
     print(f'Topic {idx}')
-    print(' '.join([feature_names[i] for i in topic.argsort()[:-n_top_words-1:-1]]))
+    print(' '.join([feature_names[i] for i in topic.argsort()[:-n_top_words-1:-1]]))  # argsort gives the order index of each word sorted from min to max, so select last index would be the most important words of each topic
 
 '''
 validate
@@ -34,7 +34,7 @@ Topic 9
 horror sex gore fans scary
 '''
 
-horror = x_topics[:,9].argsort()[::-1]
+horror = x_topics[:,9].argsort()[::-1]  # x_topics[:,n] would be the possibility that the doc belongs to topic n. use argsort+reverse to get the docs with highest possibility.
 for idx, moview_idx in enumerate(horror[:3]):
     print(f'Horror movie #{idx}')
     print(df.review[moview_idx][:300],'...')
